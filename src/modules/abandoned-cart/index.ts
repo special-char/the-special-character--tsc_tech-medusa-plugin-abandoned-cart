82ade39449b5550e4d563c4b1cce8ff7dbe31c4d
Jil Sejpal <jilsejpal7@gmail.com>
2025-04-22 15:39:19 +0530

    Update package version to 0.0.5 and change module name from 'abandoned-cart' to 'abandoned_cart' in index.ts.

--- src/modules/abandoned-cart/index.ts
+++ src/modules/abandoned-cart/index.ts
@@ -1,7 +1,7 @@
 import { Module } from "@medusajs/framework/utils";
 import AbandonedCartService from "./service";
 
-export const ABANDONED_CART_MODULE = "abandoned-cart";
+export const ABANDONED_CART_MODULE = "abandoned_cart";
 
 export default Module(ABANDONED_CART_MODULE, {
   service: AbandonedCartService,
